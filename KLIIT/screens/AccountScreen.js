72ba4ef50a7f4a89599a6424241f1497c42afebc
Anya <anya.p.nguyen@gmail.com>
2019-05-29 14:14:01 -0700

    styles

--- KLIIT/screens/AccountScreen.js
+++ KLIIT/screens/AccountScreen.js
@@ -26,9 +26,10 @@ export default class AccountScreen extends React.Component {
       >
         <View style={styles.headerWrapper}>
           <View>
-            <Text>Crystal Adams</Text>
+            <Text style={styles.headerText}>Crystal</Text>
+            <Text style={styles.headerText}>Adams</Text>
           </View>
-          <View>
+          <View style={styles.imageStyle}>
             <Text>Image</Text>
           </View>
         </View>
@@ -51,8 +52,7 @@ const styles = StyleSheet.create({
   },
   headerWrapper: {
     flex: 1,
-    backgroundColor: "yellow",
-    // backgroundColor: "#fafafa",
+    backgroundColor: "#fafafa",
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
@@ -70,87 +70,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center"
   },
-
-  developmentModeText: {
-    marginBottom: 20,
-    color: "rgba(0,0,0,0.4)",
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: "center"
+  headerText: {
+    fontSize: 36,
+    color: "#333333"
   },
-  contentContainer: {
-    paddingTop: 50
-  },
-  welcomeContainer: {
-    alignItems: "center",
-    marginTop: 10,
-    marginBottom: 20
-  },
-  welcomeImage: {
+  imageStyle: {
+    backgroundColor: "#00267d",
     width: 100,
-    height: 80,
-    resizeMode: "contain",
-    marginTop: 3,
-    marginLeft: -10
-  },
-  getStartedContainer: {
-    alignItems: "center",
-    marginHorizontal: 50
-  },
-  homeScreenFilename: {
-    marginVertical: 7
-  },
-  codeHighlightText: {
-    color: "rgba(96,100,109, 0.8)"
-  },
-  codeHighlightContainer: {
-    backgroundColor: "rgba(0,0,0,0.05)",
-    borderRadius: 3,
-    paddingHorizontal: 4
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    lineHeight: 24,
-    textAlign: "center"
-  },
-  tabBarInfoContainer: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: "black",
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3
-      },
-      android: {
-        elevation: 20
-      }
-    }),
-    alignItems: "center",
-    backgroundColor: "#fbfbfb",
-    paddingVertical: 20
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    textAlign: "center"
-  },
-  navigationFilename: {
-    marginTop: 5
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: "center"
-  },
-  helpLink: {
-    paddingVertical: 15
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: "#2e78b7"
+    height: 100
   }
 });
